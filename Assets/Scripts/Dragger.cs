5dbac5253e130751995ede41ee2e88cab91ce17b
alan.stewart@chi.ac.uk <alan.stewart@chi.ac.uk>
2024-08-19 09:46:12 +0100

    Early attempts at dragging pt.2 - draggable object returns to origPos on mouse btn release

--- Assets/Scripts/Dragger.cs
+++ Assets/Scripts/Dragger.cs
@@ -6,8 +6,11 @@ public class Dragger : MonoBehaviour
 {
     private Vector3 dragOffset;
     private float speed = 10f;
+    private Vector3 originalPos;
 
     [SerializeField] Camera cam;
+
+    private bool isDragging = false;
 
     Vector3 GetMousePos()
     {
@@ -16,13 +19,42 @@ public class Dragger : MonoBehaviour
         return mousePos;
     }
 
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+
     private void OnMouseDown()
     {
         dragOffset = transform.position - GetMousePos();
-    }
+        isDragging = true;
+
+    }    
 
     private void OnMouseDrag()
     {
         transform.position = Vector3.MoveTowards(transform.position, GetMousePos() + dragOffset, speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        print(isDragging);
+        if(Input.GetMouseButtonUp(0) && isDragging)
+        {
+            isDragging = false;
+            StartCoroutine(SnapBackToOriginalPos());
+        }
+    }
+
+    IEnumerator SnapBackToOriginalPos()
+    {
+        while(Vector3.Distance(transform.position, originalPos) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, originalPos, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = originalPos;
+
     }
 }
