02bdfcf12e96fb5d678b8680088720fbf0522be9
alan.stewart@chi.ac.uk <alan.stewart@chi.ac.uk>
2025-05-23 16:33:35 +0100

    Added visual/aural feedback for item placement in cauldron

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DropZone : MonoBehaviour, ICardDropArea
 {
+    [SerializeField] GameObject steamVFX;
+    [SerializeField] Transform child;
+
     public void OnCardDrop(Draggable card)
     {
         GameManager.Instance.RegisterDrop(card.isCorrectItem);
 
+        GameObject smoke = Instantiate(steamVFX, child.transform.position, Quaternion.identity);
+        Destroy(smoke, 2f);
         card.linkedImage.SetActive(true);
         Destroy(card.gameObject);
         card.transform.position = transform.position;
