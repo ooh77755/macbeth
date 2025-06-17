e9f0bea797b375d4d0fdbd3014591a9d90912e99
alan.stewart@chi.ac.uk <alan.stewart@chi.ac.uk>
2025-06-17 14:06:20 +0100

    0/4 drops, etc.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Fungus;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,16 +18,24 @@ public class GameManager : MonoBehaviour
     public GameObject arrow;
     public GameObject thanksBlock;
     public GameObject tutCanvas;
+    public TextMeshProUGUI dropsText;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+        dropsText.text = $"{totalDrops}/{winThreshold}";
     }
 
     public void RegisterDrop(bool isCorrect)
     {
         totalDrops++;
+
+        if(dropsText.text != null)
+        {
+            dropsText.text = $"{totalDrops}/{winThreshold}";
+
+        }
 
         if (isCorrect)
         {
